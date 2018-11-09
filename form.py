7ef1df8716faa162090e88465083a1c03544a200
Garret <gmannoce@hotmail.com>
2018-11-09 15:13:03 -0800

    3page init

---
+++ form.py
@@ -0,0 +1,8 @@
+from flask import Flask
+
+purpose of this pyfile
+
+validate data (if cannot by HTML alone)
+accept data, store in excel, send me an email with such data
+redirect user to a confirmation page using Flask
+
